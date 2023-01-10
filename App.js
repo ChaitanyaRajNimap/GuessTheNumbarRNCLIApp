c7387ea0d3b8114c344f56b1b691fa4f23c573d7
ChaitanyaRajNimap <chaitanyarajiwade@nimapinfotech.com>
2023-01-10 09:54:17 +0530

    Master initial commit

---
+++ App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import {View, Text} from 'react-native';
+
+function App() {
+  return (
+    <View>
+      <Text>Guess The Number App</Text>
+    </View>
+  );
+}
+
+export default App;
